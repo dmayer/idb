2349a831cbed7e30d4eeb3c3a2faa96bb2178b21
Daniel A. Mayer <daniel@matasano.com>
2014-01-30 17:34:56 -0600

    Experimantal branch: force utf8 encoding keychain

--- lib/keychain_plist_parser.rb
+++ lib/keychain_plist_parser.rb
@@ -7,7 +7,9 @@ class KeychainPlistParser
 
   def initialize plist_path
     $log.info 'Parsing keychain plist file..'
-    @entries = Plist4r.open(plist_path)["Array"]
+    data = File.open(plist_path,"r").read
+    data_utf8 = data.encode('utf-8', :invalid => :replace, :undef => :replace, :replace => '_')
+    @entries = Plist4r.new( :from_string => data_utf8)["Array"]
   end
 
 
