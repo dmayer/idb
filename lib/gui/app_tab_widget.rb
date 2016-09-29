9ec7bf0ec6d87732bab7171218bc95aa54efcd3d
Tomek Rabczak <tomek.rabczak@gmail.com>
2016-09-28 23:03:04 -0500

    no offenses for lib/gui/app_tab_widget.rb

--- lib/gui/app_tab_widget.rb
+++ lib/gui/app_tab_widget.rb
@@ -4,46 +4,43 @@ module Idb
     signals "app_changed()"
     signals "binary_analyzed()"
 
-    def initialize *args
-      super *args
+    def initialize(*args)
+      super(*args)
 
       @layout = Qt::GridLayout.new self
       setLayout(@layout)
 
-
-
-      @layout.addWidget @select_app_button, 0,0
+      @layout.addWidget @select_app_button, 0, 0
 
       # Box for App details
       @app_details = AppDetailsGroupBox.new @central_widget
-      @app_details.connect(SIGNAL(:show_device_status)) {
+      @app_details.connect(SIGNAL(:show_device_status)) do
         @device_status = DeviceStatusDialog.new
         @device_status.exec
-      }
+      end
 
-
-      @layout.addWidget @app_details, 0,0,2,1
+      @layout.addWidget @app_details, 0, 0, 2, 1
 
       # Entitlements
       @app_entitlements = AppEntitlementsGroupBox.new @central_widget
-      @layout.addWidget @app_entitlements, 2,0, 1, 2
+      @layout.addWidget @app_entitlements, 2, 0, 1, 2
 
       # App Binary Details
       @app_binary = AppBinaryGroupBox.new @central_widget
-      @layout.addWidget @app_binary, 0,1
-      @app_binary.connect(SIGNAL('binary_analyzed()')) {
-        emit binary_analyzed()
-      }
+      @layout.addWidget @app_binary, 0, 1
+      @app_binary.connect(SIGNAL('binary_analyzed()')) do
+        emit binary_analyzed
+      end
 
-      @spacer_horizontal = Qt::SpacerItem.new 0,1, Qt::SizePolicy::Expanding, Qt::SizePolicy::Fixed
+      @spacer_horizontal = Qt::SpacerItem.new 0, 1,
+                                              Qt::SizePolicy::Expanding,
+                                              Qt::SizePolicy::Fixed
       @layout.addItem @spacer_horizontal, 0, 3
 
-      @spacer = Qt::SpacerItem.new 0,1, Qt::SizePolicy::Fixed, Qt::SizePolicy::Expanding
-      @spacer2 = Qt::SpacerItem.new 0,1, Qt::SizePolicy::Fixed, Qt::SizePolicy::Expanding
+      @spacer = Qt::SpacerItem.new 0, 1, Qt::SizePolicy::Fixed, Qt::SizePolicy::Expanding
+      @spacer2 = Qt::SpacerItem.new 0, 1, Qt::SizePolicy::Fixed, Qt::SizePolicy::Expanding
 
       @layout.addItem @spacer, 3, 0
-
-
     end
 
     def app_changed
@@ -51,8 +48,6 @@ module Idb
       @app_details.app_changed
       @app_entitlements.clear
       @app_entitlements.app_changed
-
     end
-
   end
 end
