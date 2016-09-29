18028770752679e29f630e5fd1a1d1b465352fd6
Daniel A. Mayer <daniel@matasano.com>
2016-09-29 13:35:25 -0500

    no offenses for lib/lib/local_operations.rb

--- lib/lib/local_operations.rb
+++ lib/lib/local_operations.rb
@@ -1,67 +1,59 @@
 module Idb
   class LocalOperations
-
-
-    def file? path
+    def file?(path)
       File.file? path
     end
 
-
-    def download a, b
-      FileUtils.copy_file(a,b)
-      return true
+    def download(a, b)
+      FileUtils.copy_file(a, b)
+      true
     end
 
-
-    def directory? path
-     File.directory? path
+    def directory?(path)
+      File.directory? path
     end
 
-    def mtime path
+    def mtime(path)
       File.mtime path
     end
 
-    def open path
-      begin
-        Launchy.open path
-        return true
-      rescue Exception => e
-        $log.error "Could not open file #{path}: #{e.message}"
-        $log.error e.backtrace
-        return false
-      end
+    def open(path)
+      Launchy.open path
+      return true
+    rescue StandardError => e
+      $log.error "Could not open file #{path}: #{e.message}"
+      $log.error e.backtrace
+      return false
     end
 
-    def list_dir path
+    def list_dir(path)
       Dir.entries path
     end
 
-    def list_dir_full path
+    def list_dir_full(path)
       Dir.entries path
     end
 
-    def dir_glob path, pattern
+    def dir_glob(path, pattern)
       full_path = "#{path}/#{pattern}"
       Dir.glob(full_path)
     end
 
-    def file_exists? file
-      File.exists? file
+    def file_exists?(file)
+      File.exist? file
     end
 
-    def execute cmd
+    def execute(cmd)
       `#{cmd}`
     end
 
-    def execute_fork cmd
+    def execute_fork(cmd)
       (pid = fork) ? Process.detach(pid) : exec(cmd)
     end
 
-    def launch_app command, app
+    def launch_app(command, app)
       $log.info "Executing #{command} #{app}"
-      self.execute_fork("#{command} \"#{app}\"")
+      execute_fork("#{command} \"#{app}\"")
     end
-
-
   end
 end
