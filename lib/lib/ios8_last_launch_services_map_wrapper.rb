893b1d2f112bf13cd5a66274f18b6589f84792d9
Daniel A. Mayer <daniel@matasano.com>
2016-09-29 12:58:46 -0500

    no offenses for lib/lib/ios8_last_launch_services_map_wrapper.rb

--- lib/lib/ios8_last_launch_services_map_wrapper.rb
+++ lib/lib/ios8_last_launch_services_map_wrapper.rb
@@ -1,32 +1,25 @@
 require 'plist4r'
 
 class IOS8LastLaunchServicesMapWrapper
-
-  def initialize plist_file
+  def initialize(plist_file)
     @plist_file = plist_file
 
     @plist_data = Plist4r.open @plist_file
   end
 
-  def entitlements_by_bundle_id bundle_id
-    begin
-      @plist_data.to_hash["User"][bundle_id]["Entitlements"]
-    rescue
-      $log.error "Could not read entitlements.."
-    end
+  def entitlements_by_bundle_id(bundle_id)
+    @plist_data.to_hash["User"][bundle_id]["Entitlements"]
+  rescue
+    $log.error "Could not read entitlements.."
   end
 
-
-  def data_path_by_bundle_id bundle_id
+  def data_path_by_bundle_id(bundle_id)
     @plist_data.to_hash["User"][bundle_id]["Container"]
   end
 
-
-  def keychain_access_groups_by_bundle_id bundle_id
-    begin
-      @plist_data.to_hash["User"][bundle_id]["Entitlements"]["keychain-access-groups"]
-    rescue
-      ""
-    end
+  def keychain_access_groups_by_bundle_id(bundle_id)
+    @plist_data.to_hash["User"][bundle_id]["Entitlements"]["keychain-access-groups"]
+  rescue
+    ""
   end
 end
