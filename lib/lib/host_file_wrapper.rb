111c3dcfe5f1a5f4bf450d5fe285f206bd0f8dcd
Daniel A. Mayer <daniel@matasano.com>
2016-09-29 12:55:20 -0500

    no offenses for lib/lib/host_file_wrapper.rb

--- lib/lib/host_file_wrapper.rb
+++ lib/lib/host_file_wrapper.rb
@@ -1,6 +1,5 @@
 module Idb
   class HostFileWrapper
-
     def initialize
       @cache_path = "#{$tmp_path}/device/hosts"
     end
@@ -9,19 +8,18 @@ module Idb
       FileUtils.mkpath "#{$tmp_path}/device" unless File.directory? "#{$tmp_path}/device"
       $device.ops.download "/etc/hosts", @cache_path
       begin
-        File.open(@cache_path,"r").read
+        File.open(@cache_path, "r").read
       rescue
-
+        $log.error "Could not open #{@cache_path}"
       end
     end
 
-    def save text
+    def save(text)
       # upload
-      File.open(@cache_path,"w") { |f|
+      File.open(@cache_path, "w") do |f|
         f.puts text
-      }
+      end
       $device.ops.upload(@cache_path, "/etc/hosts")
-
     end
   end
 end
