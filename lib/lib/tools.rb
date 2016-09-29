c06c69bef99d81b63db9150703092bb62c010245
Daniel A. Mayer <daniel@matasano.com>
2016-09-29 15:53:59 -0500

    no offense in lib/lib/tools.rb

--- lib/lib/tools.rb
+++ lib/lib/tools.rb
@@ -2,10 +2,10 @@
 def which(cmd)
   exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
   ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-    exts.each { |ext|
+    exts.each do |ext|
       exe = File.join(path, "#{cmd}#{ext}")
       return exe if File.executable? exe
-    }
+    end
   end
-  return nil
+  nil
 end
