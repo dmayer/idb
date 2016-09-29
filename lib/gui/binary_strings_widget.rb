ad27f7e4982110cc5358717dbbbb90c18ffad364
Tomek Rabczak <tomek.rabczak@gmail.com>
2016-09-28 23:04:25 -0500

    no offenses for lib/gui/binary_strings_widget.rb

--- lib/gui/binary_strings_widget.rb
+++ lib/gui/binary_strings_widget.rb
@@ -1,33 +1,25 @@
 module Idb
   class BinaryStringsWidget < Qt::Widget
-
-    def initialize *args
-      super *args
+    def initialize(*args)
+      super(*args)
       @layout = Qt::GridLayout.new
       setLayout(@layout)
 
       @details = Qt::PlainTextEdit.new
       @details.setReadOnly(true)
 
-
       @extract = Qt::PushButton.new "Extract Strings"
-      @extract.connect(SIGNAL :released) {
+      @extract.connect(SIGNAL(:released)) do
         @details.clear
         strings = $selected_app.strings
         @details.appendPlainText(strings)
-      }
+      end
 
-      @layout.addWidget @details, 0,0
-      @layout.addWidget @extract, 1,0
-
-
-
-
+      @layout.addWidget @details, 0, 0
+      @layout.addWidget @extract, 1, 0
     end
 
     def refresh
-
     end
-
   end
 end
